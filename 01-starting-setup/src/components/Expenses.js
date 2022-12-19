a0f79553adce21d94232e0f8d11f24e0347903ab
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-19 14:50:09 +0200

    new structure

---
+++ 01-starting-setup/src/components/Expenses.js
@@ -0,0 +1,25 @@
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+
+function Expenses (props){
+    return (
+        <div>
+          <h2>Fitness expenses</h2>
+          <ExpenseItem
+            title={expenses[0].title}
+            amount={expenses[0].amount}
+            date={expenses[0].date}
+          ></ExpenseItem>
+          <ExpenseItem
+            title={expenses[1].title}
+            amount={expenses[1].amount}
+            date={expenses[1].date}
+          ></ExpenseItem>
+          <ExpenseItem
+            title={expenses[2].title}
+            amount={expenses[2].amount}
+            date={expenses[2].date}
+          ></ExpenseItem>
+        </div>
+      );
+}
