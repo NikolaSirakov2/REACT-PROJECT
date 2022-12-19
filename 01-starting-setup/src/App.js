00de97d44f67a00fcbd3be1710393d03a7f4deda
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-19 10:47:03 +0200

    Expense function

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,8 +1,10 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Hello! My name is Nikola!</h2>
-      <p>This is my new project.</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>December 20th 2022</div>
+      <h2>Fitness Membership</h2>
+      <div>$50.00</div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
