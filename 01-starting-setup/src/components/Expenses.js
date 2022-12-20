f3b24a2e725bd98bd23b449ec4be4517e82883fd
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-20 09:12:42 +0200

    update

--- 01-starting-setup/src/components/Expenses.js
+++ 01-starting-setup/src/components/Expenses.js
@@ -1,25 +1,27 @@
-import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem";
 
-function Expenses (props){
-    return (
-        <div>
-          <h2>Fitness expenses</h2>
-          <ExpenseItem
-            title={expenses[0].title}
-            amount={expenses[0].amount}
-            date={expenses[0].date}
-          ></ExpenseItem>
-          <ExpenseItem
-            title={expenses[1].title}
-            amount={expenses[1].amount}
-            date={expenses[1].date}
-          ></ExpenseItem>
-          <ExpenseItem
-            title={expenses[2].title}
-            amount={expenses[2].amount}
-            date={expenses[2].date}
-          ></ExpenseItem>
-        </div>
-      );
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(props) {
+  return (
+    <div className="expenses">
+    <ExpenseItem
+      title={props.items[0].title}
+      amount={props.items[0].amount}
+      date={props.items[0].date}
+    ></ExpenseItem>
+    <ExpenseItem
+      title={props.items[1].title}
+      amount={props.items[1].amount}
+      date={props.items[1].date}
+    ></ExpenseItem>
+    <ExpenseItem
+      title={props.items[2].title}
+      amount={props.items[2].amount}
+      date={props.items[2].date}
+    ></ExpenseItem>
+  </div>
+  )
 }
+
+export default Expenses;
